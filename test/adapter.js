0d875a948678a5c66be67e77b40f9847c98aa98c
Marcos Bérgamo <markusdarkness@gmail.com>
2016-02-29 11:15:37 -0300

    Add bootstrap files

---
+++ test/adapter.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let internals = {};
+
+module.exports = internals.Adapter = function (options) {};
+
+internals.Adapter.attributes = {
+  name: 'Example',
+  version: '1.0.0'
+};
+
+internals.Adapter.prototype.load = function () {
+  return {};
+}
---
+++ test/index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('lab');
+const Code = require('code');
+const Hapi = require('hapi');
+
+// Load example
+
+const K7 = require('../lib/k7');
+const Adapter = require('./adapter');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const it = lab.it;
+const expect = Code.expect;
+const describe = lab.describe;
+
+describe('K7', () => {
+  let server;
+  let options = {
+    connectionString: 'mongodb://localhost:27017/k7',
+    models: 'test/**/model.js',
+    adapter: Adapter
+  };
+
+  it('should register the plugin', (done) => {
+    server = new Hapi.Server();
+    const register = {
+      register: require('../index'),
+      options: options
+    };
+
+    server.register([register], (err) => {
+      // No errors
+
+      expect(err).to.not.exist;
+
+      // Instance of K7 should be registered
+
+      expect(server).to.deep.include('database');
+      expect(server.database).to.be.an.object();
+
+      done();
+    });
+  }); 
+
+  it('should register the plugin with an array of models in options', (done) => {
+    server = new Hapi.Server();
+
+    options.models = [options.models];
+
+    const register = {
+      register: require('../index'),
+      options: options
+    };
+
+    server.register([register], (err) => {
+      // No errors
+
+      expect(err).to.not.exist;
+
+      // Instance of K7 should be registered
+
+      expect(server).to.deep.include('database');
+      expect(server.database).to.be.an.object();
+
+      done();
+    });
+  
+  });
+
+
+  it('should register the plugin with adapter as string', (done) => {
+    server = new Hapi.Server();
+
+    options.adapter = '../test/adapter';
+
+    const register = {
+      register: require('../index'),
+      options: options
+    };
+
+    server.register([register], (err) => {
+      // No errors
+
+      expect(err).to.not.exist;
+
+      // Instance of K7 should be registered
+
+      expect(server).to.deep.include('database');
+      expect(server.database).to.be.an.object();
+
+      done();
+    });
+  
+  });
+
+});
